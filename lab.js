bcf9f7d62ed26ac46ff8df07c9ce8c39db44a8b7
Kody Wells <kodyswells@gmail.com>
2024-01-13 11:07:52 -0700

    Solved problem 6

--- lab.js
+++ lab.js
@@ -121,7 +121,9 @@ for (var i = 0; i < 10; i++) {
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+for(var i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
 ////////// PROBLEM 7 //////////
 
